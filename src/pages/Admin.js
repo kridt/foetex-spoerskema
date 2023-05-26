b7285e4aab90e5f8e27264d50575efd677bea36a
kridt <69456983+kridt@users.noreply.github.com>
2023-05-26 23:58:48 +0200

    admin site

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
-import Chart from "react-google-charts";
+import React, { useEffect, useState } from "react";
+import { firestore } from "../firebase";
+/* import Chart from "react-google-charts"; */
 
 export default function Admin() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [allAnswers, setAllAnswers] = useState([]);
   const [morning, setMorning] = useState([]);
   const [evening, setEvening] = useState([]);
+  /* const [loading, setLoading] = useState(false); */
 
   /* const thatData = [
     ["Kundeoplevelser gns", "Aften", "Morgen"],
@@ -41,9 +42,47 @@ export default function Admin() {
     },
   };
  */
+
+  useEffect(() => {
+    firestore
+      .collection("allAnswers")
+      .doc("morning")
+      .collection("answers")
+      .get()
+      .then((snapshot) => {
+        setMorning(snapshot.docs.map((doc) => doc.data()));
+      });
+
+    firestore
+      .collection("allAnswers")
+      .doc("evening")
+      .collection("answers")
+      .get()
+      .then((snapshot) => setEvening(snapshot.docs.map((doc) => doc.data())));
+
+    setAllAnswers([...morning, ...evening]);
+  }, []);
+
+  console.log(allAnswers);
   return (
     <div>
       <h1>Admin og statistik</h1>
+
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+          <div
+            className="morgenStats"
+            style={{ borderRight: "1px white solid ", paddingRight: "1em" }}
+          >
+            <h3 style={{ textAlign: "center" }}>Morgen</h3>
+            <h3>Antal svar 7-15: {morning?.length}</h3>
+          </div>
+          <div className="aftenStats" style={{ paddingLeft: "1em" }}>
+            <h3 style={{ textAlign: "center" }}>Aften</h3>
+            <h3>Antal svar 15-21: {evening?.length}</h3>
+          </div>
+        </div>
+      </div>
       {/* <Chart
         chartType="BarChart"
         data={thatData}
